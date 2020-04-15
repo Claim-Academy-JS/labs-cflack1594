c583e548e850325fb3f7f6b27d3380ed327f642e
cflack1594 <cflack1594@gmail.com>
2020-04-15 18:58:10 -0500

    ADD 4/15 number sum lab solutions for tasks 1-4

--- index.js
+++ index.js
@@ -1,22 +1,64 @@
-function createFizzBuzz(fizzNum = 3, buzzNum = 5) {
-  const fizzBuzzArr = [];
+/*
 
-  for (let i = 1; i <= 100; i++) {
-    if (i % fizzNum === 0 && i % buzzNum === 0) {
-      fizzBuzzArr.push("Fizz");
-    } else if (i % buzzNum === 0) {
-      fizzBuzzArr.push("Buzz");
-    } else if (i % fizzNum) {
-      fizzBuzzArr.push("Fizz. Buzz");
+1. Update the example above so that it *returns* a *string* like so, **`The total of the numbers is:`.
+2. Write another *function* that takes in an *array* of numbers. This one *invokes* `sumDaNumbers`  and uses that *return value* to calculate the average (HINT: Use `length`).
+3. Update your *function* so that if there are any 'non-numbers' in the *array,* they are 'skipped.' (HINT: you could use `[continue](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/continue)`).
+4. Update your *function* so that `if` there are 'non-strings' involved, it adds that count to the `return`.
+
+*/
+
+// Sum the numbers in this ARRAY.
+const nums = [51, 22, 33, 44, 511, 65, 7, 8, 9, 10];
+let nonNumCount = 0;
+
+function sumDaNumbers(nums) {
+  let total = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (typeof nums[i] === "number") {
+      total += nums[i];
     } else {
-      fizzBuzzArr.push(i);
+      nonNumCount++;
     }
   }
 
-  return fizzBuzzArr;
+  return total;
 }
 
-const fizzBuzzResult = [];
-fizzBuzzResult.push(createFizzBuzz(2, 6));
+function getAverage(nums) {
+  let average = 0,
+    tempTotal = 0;
 
-console.log(fizzBuzzResult);
+  tempTotal = sumDaNumbers(nums);
+  average = tempTotal / (nums.length - nonNumCount);
+
+  return average;
+}
+
+function makeString(...args) {
+  let outputString = "";
+
+  for (let i = 0; i < args.length; i++) {
+    outputString += args[i];
+  }
+
+  return outputString;
+}
+
+const totalOfNums = sumDaNumbers(nums);
+const averageOfNums = getAverage(nums);
+
+const totalOfNumsString = makeString(
+  `The total is: `,
+  totalOfNums,
+  `
+The average is: `,
+  averageOfNums,
+  `
+The number of non number variables is: `,
+  nonNumCount
+);
+
+console.log(totalOfNums);
+console.log(averageOfNums);
+console.log(totalOfNumsString);
