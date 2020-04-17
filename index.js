4a0e5be3a3b17ceebae10871170fa696bb91d65c
cflack1594 <cflack1594@gmail.com>
2020-04-17 18:10:30 -0500

    ADD Lab find age in years

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-//take birthday and calculate age in days
+//take birthday and calculate age in years
 function numberOfDaysOld(birthday) {
   const bday = new Date(birthday);
   const now = new Date(Date.now());
   return msToDays(now - Date.parse(bday));
+}
+
+function calcYearsOld(dob) {
+  const dobDate = new Date(dob);
+  const nowDate = new Date();
+
+  return nowDate.getFullYear() - dobDate.getFullYear();
 }
 
 function msToDays(ms) {
@@ -11,5 +18,7 @@ function msToDays(ms) {
 
 const birthday = "April 29 1999";
 const daysOld = numberOfDaysOld(birthday);
+const yearsOld = calcYearsOld(birthday);
 
 console.log(`You are ${daysOld.toFixed(0)} days old`);
+console.log(`You are ${yearsOld.toFixed(0)} years old`);
